5fbdbfacac08d281122b5621c3dd754df4ac0554
Eason Tsui <hibfnv@hotmail.com>
2018-09-17 11:34:15 +0800

    Add files via upload

---
+++ rock_settings.py
@@ -0,0 +1,6 @@
+class Rocksets:
+    def __init__(self):
+        self.screen_width = 1024
+        self.screen_height = 768
+        self.bg_color = (180, 160, 210)
+        self.rock_speed_factor = 1.5
