a8a37da41f7e2403aec58d887c64f77284cd599d
Eason Tsui <hibfnv@hotmail.com>
2018-09-17 08:47:57 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,4 +2,5 @@ class Settings:
     def __init__(self):
         self.screen_width = 800
         self.screen_height = 600
-        self.bg_color = (228, 231, 223)
+        self.bg_color = (210, 220, 213)
+        self.ship_speed_factor = 1.5
