bab4589473394c433c9458b9d28beec7d8a5b69c
Eason Tsui <hibfnv@hotmail.com>
2018-09-28 11:33:58 +0800

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,21 +4,24 @@ from settings import Settings
 
 from ship import Ship
 
+import game_function as gf
 
-import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
-    #  初始化屏幕
     pygame.init()
-    ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    ai_setting = Settings()
+    screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    ship = Ship(screen)
+    ship = Ship(ai_setting, screen)
+    bullets = Group()
+
     while True:
-        # 监控键盘鼠标事件
-        gf.check_events(ship)
-        gf.update_screen(ai_settings, screen, ship)
+        gf.check_events(ai_setting, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_setting, screen, ship, bullets)
 
 
 run_game()
