56a25d3509c8f9f7a6698146fe6fda6cb49f1749
Eason Tsui <hibfnv@hotmail.com>
2018-09-28 11:34:47 +0800

    Update alien_settings.py

--- alien_settings.py
+++ alien_settings.py
@@ -1,6 +1,12 @@
 class Settings:
+
     def __init__(self):
-        self.screen_width = 800
-        self.screen_height = 600
-        self.bg_color = (210, 220, 213)
-        self.ship_speed_factor = 1.5
+        self.screen_width = 1024
+        self.screen_height = 768
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullet_allowed = 3
